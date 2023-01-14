f0b7a8af55ae2e8f67f869ee80751ed45d672c8d
Ahmed Hamdy <117328582+ahmedhamdyyounis@users.noreply.github.com>
2023-01-14 14:58:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -56,3 +56,38 @@ function closeSub() {
 // End Sub-Menu Toggle
 
 
+//Start slides
+let imgOne = document.getElementById("img-one")
+let imgTwo = document.getElementById("img-two")
+let arrowLeft = document.getElementById("arrow-left")
+let arrowRight = document.getElementById("arrow-right")
+let slides = document.querySelector(".slides-wrapper")
+
+// arrowLeft.addEventListener('click', function () {
+//     imgOne.style.marginLeft = '-366px'
+
+// })
+
+// arrowRight.addEventListener('click', function () {
+//     imgOne.style.marginLeft = '0'
+// })
+
+arrowRight.onclick = function () {
+    imgOne.style.marginLeft = '100%'
+    imgTwo.style.marginLeft = '-200%'
+
+}
+var clickCounter = 0;
+arrowLeft.onclick= function() {
+clickCounter++;
+    if (clickCounter==1) {
+    imgOne.style.marginLeft = '-100%'
+    } else if (clickCounter==2) {
+        imgTwo.style.marginLeft = '-100%'
+        imgOne.style.marginLeft = '0'
+        
+    } else {
+        imgTwo.style.marginLeft = '-200%'
+    }
+   };
+//End slides
